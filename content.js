3c961ea7bca75d8a7df789bdeaac54a4cd314a28
Sawan <sawan.dasari@gmail.com>
2025-05-13 14:24:08 -0400

    Changes made to name and email in JS

--- content.js
+++ content.js
@@ -4,8 +4,8 @@
     const inputEmail = document.querySelector('input[name="email"]');
     const inputResume = document.querySelector('input[type="file"]');
   
-    if (inputName) inputName.value = "John Doe";
-    if (inputEmail) inputEmail.value = "john@example.com";
+    if (inputName) inputName.value = "Sawan Dasari";
+    if (inputEmail) inputEmail.value = "sawandsr@gmail.com";
     if (inputResume) {
       // File upload cannot be done with JS alone for security reasons.
       console.log("Please upload resume manually.");
